a3dd11d043cca9510010fcbdaf31cf2b27d4b298
Nicolas Jandre <nicolasjandre@live.com>
2022-12-05 16:33:23 -0300

    adding index.html with a menu of all projects

--- js/index.js
+++ js/index.js
@@ -1,18 +1,21 @@
 const a = document.querySelectorAll('a')
 const lists = document.querySelectorAll('li')
 
-a.forEach(a => a.addEventListener('mouseover', unhiddenImage))
-a.forEach(a => a.addEventListener('mouseleave', hiddenImage))
+a.forEach(a => a.addEventListener('mouseenter', unhiddeImage))
+a.forEach(a => a.addEventListener('mouseleave', hideImage))
 
 
-function unhiddenImage(event) {
+function unhiddeImage(event) {
     lists.forEach(li => {
-        if (li.innerText !== event.relatedTarget.innerText) return
+        if (li.innerText !== event.relatedTarget.innerText) {
+            li.classList.remove('unhidden')
+            return
+        }
         li.classList.add('unhidden')
     })
 }
 
-function hiddenImage(event) {
+function hideImage(event) {
     lists.forEach(li => {
         if (li.innerText !== event.relatedTarget.innerText) return
         li.classList.remove('unhidden')
